d9591e6a97876b59d71cbaf910bcd0ad273baf6e
afridahmed92-cpu <afridahmed92@gmail.com>
2026-08-16 12:08:43 +0400

    Add files via upload

--- app/cms-config.ts
+++ app/cms-config.ts
@@ -1,4 +1,4 @@
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://tbogzwsrnjqznnllilla.supabase.co";
 export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || "sb_publishable_uN5LqGpRwea6DAogc7gqHA_rBmOya3-";
-export const CMS_ADMIN_EMAIL = "afridahamed110@gmail.com";
+export const CMS_ADMIN_EMAIL = "admin@fresh-kind.com";
 export const CMS_BUCKET = "site-assets";
